ba6eda36379d7b413bee2097f85baec8c62ff95d
Diego Silva <dsilva2401@gmail.com>
2016-03-14 10:07:00 -0500

    ngCordova added

--- www/src/app/app.js
+++ www/src/app/app.js
@@ -1,6 +1,6 @@
 (function (ang) {
   
-	var app = ang.module('app', [ 'ionic', 'resources', 'ui.router' ]);
+	var app = ang.module('app', [ 'ionic', 'resources', 'ui.router', 'ngCordova' ]);
 
 })(angular);
 
